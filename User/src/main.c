dff3d2a4f75b0141c85c12012ac0876924c05c76
zhzhongshi <zhz894524@163.com>
2024-05-25 19:18:29 +0800

    点个灯

--- User/src/main.c
+++ User/src/main.c
@@ -13,7 +13,7 @@ void GPIO_Configuration(void)
     GPIO_InitStructure.GPIO_Pin =  GPIO_Pin_13;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(GPIOB, &GPIO_InitStructure);
+    GPIO_Init(GPIOC, &GPIO_InitStructure);
 }
 
 int main() {
